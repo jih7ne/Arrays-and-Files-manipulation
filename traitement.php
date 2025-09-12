d16ea4f65140b4fa3d4b479a27a6a61460c85ccf
jih7ne <jianmercley@gmail.com>
2025-09-12 15:56:26 +0000

    suppression des doublons

--- traitement.php
+++ traitement.php
@@ -5,4 +5,22 @@ function validerEmail($email){
     $reg = "/^[a-zA-Z0-9._-]+@[a-z0-9._-]{2,}\.[a-z]{2,4}$/"; //regex pour un email valide
    return preg_match($reg, $email);
 }
+
+//Fonction pour supprimer les doublons
+function supprimerDoublons($T){
+$new = [];
+foreach($T as $email) {
+    $existe = false;
+    foreach($new as $element){
+        if($element == $email){
+            $existe = true;
+            break;
+        }
+    }
+    if($existe ==false){
+        $new[] = $email;
+    }
+}
+return $new;
+}
 ?>
